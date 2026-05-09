878829d60277ff01a69b2f354dc93327ca282138
Sangin <silee9019@gmail.com>
2026-05-09 17:41:00 +0900

    fix(appback): version 라인을 url 보다 먼저 두기 (Ruby 인터폴레이션 순서)

--- Formula/appback.rb
+++ Formula/appback.rb
@@ -7,9 +7,9 @@
 class Appback < Formula
   desc "Mac app settings backup & restore CLI"
   homepage "https://github.com/silee-tools/cli/tree/main/apps/appback"
+  version "0.3.0"
   url "https://github.com/silee-tools/cli/releases/download/appback/v#{version}/appback-v#{version}.tar.gz"
   sha256 "60887e11592583a1245572f3f7db2470c1e15735936864dc1dd06c5b188619f0"
-  version "0.3.0"
   license "MIT"
 
   depends_on "gum"
